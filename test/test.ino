36c5d9be11536551e750fb055e6724f6e92c0ec1
Dan Sun <ds318@duke.edu>
2016-10-01 15:13:10 -0400

    added sample code for motor and magnetometer

---
+++ test/test.ino
@@ -0,0 +1,36 @@
+#include <AFMotor.h>
+
+AF_DCMotor motor2(2, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Motor test!");
+  
+  motor1.setSpeed(150);     // set the speed to 200/255
+  motor2.setSpeed(150);
+   Serial.print("tick");
+  
+  motor1.run(FORWARD);      // turn it on going forward
+  motor2.run(FORWARD);      // turn it on going forward
+  delay(1000);
+  
+}
+
+void loop() {
+//  Serial.print("tick");
+  
+//  motor1.run(FORWARD);      // turn it on going forward
+//  motor2.run(FORWARD);      // turn it on going forward
+//  delay(1000);
+
+  //Serial.print("tock");
+  //motor1.run(BACKWARD);     // the other way
+  //motor2.run(BACKWARD);     // the other way
+  //delay(1000);
+  //
+  //Serial.print("tack");
+  //motor1.run(RELEASE);      // stopped
+//  motor2.run(RELEASE);      // stopped
+//  delay(1000);
+}
